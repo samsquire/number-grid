7ccba6769efcc37c101fdb0164b58075889ed45d
samsquire <sam@samsquire.com>
2023-08-18 11:00:41 +0000

    Trying to work out how to paralellise relationships

---
+++ README.md
@@ -0,0 +1,19 @@
+# number-grids
+
+Is the behaviour of an event, another event, the outcome?
+
+Can events be created with mathematical operations, to produce new results?
+
+I'm a big fan of the work by Nathan Marz on Rama, see this blog post:
+[How we reduced the cost of building Twitter at Twitter-scale by 100x](https://blog.redplanetlabs.com/2023/08/15/how-we-reduced-the-cost-of-building-twitter-at-twitter-scale-by-100x/)
+
+I have an intuition that behaviour can be parallelised but I'm trying to work out how.
+
+I am trying to study the relationship of numbers to work out if there is a relationship that can be hooked into to provide for parallelisable behaviour.
+
+Parallelise relationships
+
+Update a field to say it's in their timeline
+
+permutation of followers
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE
+from pprint import pprint
 
 
 class Graph:
@@ -21,19 +22,21 @@ class Graph:
     graph = "digraph G {"
     for item, value in self.adjacency.items():
       for link in value:
-        graph += "\"{}\" -> \"{}\" [label=\"{}\"];".format(item, link[0], link[1])
+        graph += "\"{}\" -> \"{}\" [label=\"{}\"];".format(
+            item, link[0], link[1])
     graph += "}"
 
     dot.communicate(graph.encode("utf8"))
 
+
 drawn = {}
-count = 5
+count = 4
 g = Graph("numbers")
 for a in range(0, count):
   for b in range(0, count):
     if b == a:
       continue
-    
+
     if a not in drawn:
       drawn[a] = []
     if b in drawn[a]:
@@ -42,7 +45,61 @@ for a in range(0, count):
     # diff = a - b
     #g.add_link(a, (b, str(diff)))
     sum = a + b
-    g.add_link(a, (sum, "+"+str(b)))
+    g.add_link(a, (sum, "+" + str(b)))
 
 g.draw()
-  
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+commands = [
+    "create message", "send_message_to_follower", "mute", "follow",
+    "add_to_timeline", "unfollow", "like", "fanout", "unlike", "share",
+    "stream", "upload", "fanout", "fanout", "fanout", "fanout", "fanout",
+    "fanout", "fanout", "fanout", "fanout", "fanout", "fanout", "fanout",
+    "fanout", "fanout", "fanout"
+]
+# how do you map addition to IO?
+# need to get data to the right places in the most efficient way
+inboxes = {}
+for item in range(0, len(commands)):
+  inboxes[item] = []
+start = 0
+event = 0
+sequence = [0, 3, -3, 3, 2]
+
+
+def run(event):
+  for number in sequence:
+
+    new_event = event + number
+    print(new_event)
+    print("{} {} -> {} {}".format(event, commands[event], new_event,
+                                  commands[new_event]))
+    inboxes[new_event].append(event)
+    output = start
+    coutput = start
+    a = min(event, new_event)
+    b = max(event, new_event)
+    for intermediary in range(a, b):
+      print("{} {} -> {} {}".format(new_event, commands[new_event],
+                                    intermediary, commands[intermediary]))
+      output = "{}({})".format(alphabet[intermediary], output)
+      coutput = "{}({})".format(commands[intermediary], coutput)
+    print(output)
+    print(coutput)
+    event = new_event
+
+
+run(event)
+
+pprint(inboxes)
+
+# import itertools
+
+# user_size = 20
+# users = list(range(0, user_size))
+# permutations = list(itertools.product([0, 1], repeat=user_size))
+# print(len(permutations))
+# print(permutations)
+
+# import itertools
+# print(list(itertools.product(["A", "B", "C"], repeat=3)))
